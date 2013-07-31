f7bc49ed726250db917c4276a2f2af8ebebd305b
Bryan <bkapicka@gmail.com>
2013-07-31 11:54:06 -0400

    simplified use of gsub

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,10 +3,11 @@ class Location < ActiveRecord::Base
   # Return description without spaces and including location type if present
   def description_url
   	if self.location_type? 
-  		description_url = self.name.gsub(' ','_') + '_(' + self.location_type.gsub(' ','_') + ')'
+  		description_url = self.name + '_(' + self.location_type + ')'
     else
-    	description_url = self.name.gsub(' ','_')
+    	description_url = self.name
   	end
+  	description_url.gsub(' ','_')
   end
 
 end
